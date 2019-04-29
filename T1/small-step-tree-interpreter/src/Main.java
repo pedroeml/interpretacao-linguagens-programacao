02c42369ee9e6e12d644a22f1ca53f67862b98d8
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-04-28 23:02:12 -0300

    Sample use case

--- T1/small-step-tree-interpreter/src/Main.java
+++ T1/small-step-tree-interpreter/src/Main.java
@@ -1,12 +1,17 @@
 import environment.Environment;
-import semantic.Literal;
+import semantic.*;
 
 public class Main {
-    private static Environment<Integer> environmentState;
+    private static Environment environmentState;
 
     public static void main(String[] args) {
-        environmentState =  new Environment<>();
-        environmentState.addEntry("x", new Literal<>(123));
+        environmentState =  new Environment();
+        environmentState.addEntry("x", new Literal(10));
         System.out.println(environmentState.toString());
+
+        AbstractSyntaxTree tree = new Assign("x", new Sum(new Variable("x"), new Sum(new Literal(5), new Literal(5))));
+        AbstractSyntaxTree resultingTree = tree.smallStep(environmentState);
+        System.out.println(environmentState.toString());
+
     }
 }
