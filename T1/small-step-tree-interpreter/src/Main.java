23c599eed9dd90f3f316a6a6c0e0a8f1ce1b2e4c
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-04-29 00:24:50 -0300

    Updates on sample use case

--- T1/small-step-tree-interpreter/src/Main.java
+++ T1/small-step-tree-interpreter/src/Main.java
@@ -1,6 +1,8 @@
 import environment.Environment;
 import semantic.*;
 import semantic.arith.Literal;
+import semantic.arith.Multiply;
+import semantic.arith.Subtract;
 import semantic.arith.Sum;
 import semantic.bool.And;
 import semantic.bool.BooleanLiteral;
@@ -15,11 +17,12 @@ public class Main {
         environmentState.addEntry("x", new Literal(10));
         System.out.println(environmentState.toString());
 
-        AbstractSyntaxTree tree = new Assign("x", new Sum(new Variable("x"), new Sum(new Literal(5), new Literal(5))));
-        AbstractSyntaxTree resultingTree = tree.smallStep(environmentState);
+        AbstractSyntaxTree tree = new Assign("x", new Multiply(new Sum(new Variable("x"), new Subtract(new Literal(10), new Literal(5))), new Literal(2)));
+        tree.smallStep(environmentState).toString();
+        System.out.println(tree.toString());
         System.out.println(environmentState.toString());
 
         tree = new And(new Or(new BooleanLiteral(true), new BooleanLiteral(false)), new Not(new BooleanLiteral(false)));
-        System.out.printf(tree.toString() + " = " + tree.smallStep(environmentState).toString());
+        System.out.println(tree.toString() + " = " + tree.smallStep(environmentState).toString());
     }
 }
