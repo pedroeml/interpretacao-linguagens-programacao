3c7a5b401bf54efca281253beae99f99055e1831
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-04-30 00:53:54 -0300

    Updates on sample use case

--- T1/small-step-tree-interpreter/src/Main.java
+++ T1/small-step-tree-interpreter/src/Main.java
@@ -8,11 +8,9 @@ public class Main {
     private static Environment environmentState;
 
     public static void main(String[] args) {
-        environmentState =  new Environment();
-        environmentState.addEntry("x", new Literal(17));
-        environmentState.addEntry("y", new Literal(5));
+        environmentState = sampleEnvironment();
 
-        AbstractSyntaxTree tree = new Sequence(
+        AbstractSyntaxTree treeWhile = new Sequence(
                 new Assign("z", new Literal(0)),
                 new While(
                         new Or(
@@ -21,7 +19,26 @@ public class Main {
                         new Sequence(
                                 new Assign("z", new Sum(new Variable("z"), new Literal(1))),
                                 new Assign("x", new Subtract(new Variable("x"), new Variable("y"))))));
-        tree.smallStep(environmentState);
+        treeWhile.smallStep(environmentState);
 
+        environmentState = sampleEnvironment();
+
+        AbstractSyntaxTree treeFor = new For(
+                new Assign("z", new Literal(0)),
+                new Or(
+                        new Less(new Variable("y"), new Variable("x")),
+                        new Equals(new Variable("y"), new Variable("x"))),
+                new Assign("z", new Sum(new Variable("z"), new Literal(1))),
+                new Assign("x", new Subtract(new Variable("x"), new Variable("y"))));
+        treeFor.smallStep(environmentState);
+
+    }
+
+    private static Environment sampleEnvironment() {
+        Environment environment =  new Environment();
+        environment.addEntry("x", new Literal(17));
+        environment.addEntry("y", new Literal(5));
+
+        return environment;
     }
 }
