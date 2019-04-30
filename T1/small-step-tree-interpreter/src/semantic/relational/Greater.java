9e729d922b938adfc4eca125dcecf551d03fdddb
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-04-29 22:43:45 -0300

    Relational Operators

---
+++ T1/small-step-tree-interpreter/src/semantic/relational/Greater.java
@@ -0,0 +1,34 @@
+package semantic.relational;
+
+import environment.Environment;
+import semantic.AbstractSyntaxTree;
+import semantic.arith.ArithmeticExpression;
+import semantic.arith.Literal;
+import semantic.bool.BooleanExpression;
+import semantic.bool.BooleanLiteral;
+
+public class Greater extends BooleanExpression {
+    private ArithmeticExpression exp1;
+    private ArithmeticExpression exp2;
+
+    public Greater(ArithmeticExpression exp1, ArithmeticExpression exp2) {
+        this.exp1 = exp1;
+        this.exp2 = exp2;
+    }
+
+    @Override
+    public AbstractSyntaxTree smallStep(Environment environmentState) {
+        if (!(this.exp1 instanceof Literal)) {
+            return new Greater((Literal) this.exp1.smallStep(environmentState), this.exp2).smallStep(environmentState);
+        } else if (!(this.exp2 instanceof Literal)) {
+            return new Greater(this.exp1, (Literal) this.exp2.smallStep(environmentState)).smallStep(environmentState);
+        } else {
+            return new BooleanLiteral(((Literal) this.exp1).getNu() > ((Literal) this.exp2).getNu());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.exp1.toString() + " > " + this.exp2.toString() + ")";
+    }
+}
